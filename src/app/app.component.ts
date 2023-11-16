4ecdef5abd8036625115e2b914cc738de2abf8e2
wil-deving <wroger.gutierrez@gmail.com>
2023-11-16 19:35:09 -0400

    Adding router and modules of Material UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [BrowserModule],
 })
 export class AppComponent {
   title = 'ai-demo-ui';
