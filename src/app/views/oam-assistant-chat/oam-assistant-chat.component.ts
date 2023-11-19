77633cee3f704e54d2de46c19dcdea23311d3b38
wil-deving <wroger.gutierrez@gmail.com>
2023-11-19 01:53:02 -0400

    hdfhggdf

--- src/app/views/oam-assistant-chat/oam-assistant-chat.component.ts
+++ src/app/views/oam-assistant-chat/oam-assistant-chat.component.ts
@@ -6,6 +6,14 @@ import {
   Validators,
 } from '@angular/forms';
 import { ApiService } from 'src/app/services/api.service';
+import {
+  DomSanitizer,
+  SafeResourceUrl,
+  SafeUrl,
+} from '@angular/platform-browser';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import * as data from './b164.json';
 
 export interface MessageHistory {
   id: number;
@@ -22,7 +30,6 @@ export interface MessageHistory {
   styleUrls: ['./oam-assistant-chat.component.css'],
 })
 export class OamAssistantChatComponent implements OnInit {
-  /*
   messageHistory: MessageHistory[] = [
     {
       id: 1,
@@ -41,10 +48,10 @@ export class OamAssistantChatComponent implements OnInit {
       isUser: false,
     },
   ];
-*/
-  messageHistory: MessageHistory[] = [];
 
-  isUserKnoww: boolean = false;
+  //messageHistory: MessageHistory[] = [];
+
+  isUserKnoww: boolean = true;
   MessageFieldsform: FormGroup;
   StartFieldsform: FormGroup;
   textMessage = new FormControl('', Validators.required);
@@ -61,10 +68,21 @@ export class OamAssistantChatComponent implements OnInit {
   globalAudioChunks: any[] = [];
   globalMediaRecorder: MediaRecorder | null = null;
 
+  // Data reproduccion audio
+  filePath: any = 'D:/tmp1/test1.mp3';
+
+  rutaArchivo = 'src/app/components/views/oam-assitant-chat/b64.txt';
+
+  jsonData: any = data;
+
   constructor(
     private formBuilder: FormBuilder,
-    private apiService: ApiService
+    private apiService: ApiService,
+    private _sanitizer: DomSanitizer,
+    private http: HttpClient
   ) {
+    this.filePath =
+      this._sanitizer.bypassSecurityTrustResourceUrl('D:/tmp1/test1.mp3');
     console.log('text Form');
     this.MessageFieldsform = this.formBuilder.group(
       {
@@ -84,7 +102,16 @@ export class OamAssistantChatComponent implements OnInit {
     );
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // Puedes acceder a tu data JSON aquí
+    console.log(this.jsonData.content);
+    /*
+    this.http
+      .get(this.rutaArchivo, { responseType: 'text' })
+      .subscribe((data) => {
+        console.log('DATA', data);
+      });*/
+  }
 
   onSubmitChatMessage() {
     if (this.isVoice) {
@@ -109,6 +136,37 @@ export class OamAssistantChatComponent implements OnInit {
       this.messageHistory.push(userNewMessage);
       this.textMessage.setValue('');
 
+      this.filePath =
+        this._sanitizer.bypassSecurityTrustResourceUrl('D:/tmp1/test1.mp3');
+
+      //this.filePath = this.filePath + 'test1.mp3';
+
+      console.log('DEV', this.filePath);
+
+      console.log(this.jsonData.content);
+
+      const decodedData = atob(this.jsonData.content);
+
+      // Convertir la cadena decodificada en un array de bytes
+      const byteArray = new Uint8Array(decodedData.length);
+      for (let i = 0; i < decodedData.length; i++) {
+        byteArray[i] = decodedData.charCodeAt(i);
+      }
+
+      // Crear un Blob con el array de bytes y el tipo de archivo
+      const blob = new Blob([byteArray], { type: 'audio/mp3' });
+
+      // Puedes ahora usar el objeto Blob según tus necesidades
+      console.log(blob);
+
+      const audioPlayer = document.getElementById(
+        'audioPlayer'
+      ) as HTMLAudioElement;
+      audioPlayer.src = URL.createObjectURL(blob);
+
+      audioPlayer.play();
+
+      /*
       this.apiService.sendMessage(parameters).subscribe((data) => {
         console.log('DEV', data);
 
@@ -121,8 +179,11 @@ export class OamAssistantChatComponent implements OnInit {
           isUser: false,
         };
 
+        this.filePath = this.filePath + 'test1.mp3';
+
         this.messageHistory.push(userNewMessage);
       });
+      */
     }
   }
 
@@ -151,20 +212,27 @@ export class OamAssistantChatComponent implements OnInit {
     let audioChunks: any = [];
     let self = this;
 
+    self.globalMediaRecorder?.start();
+
     const constraints = { audio: true };
     navigator.mediaDevices
       .getUserMedia(constraints)
       .then((stream) => {
         self.globalMediaRecorder = new MediaRecorder(stream);
-
+        //self.globalMediaRecorder.start();
         self.globalMediaRecorder.ondataavailable = (event) => {
           console.log('DEV on event');
-
           if (event.data.size > 0) {
             this.globalAudioChunks.push(event.data);
           }
         };
-        self.globalMediaRecorder.start();
+        self.globalMediaRecorder.onstop = () => {
+          console.log('onStop');
+          const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
+          //audioPlayer.src = URL.createObjectURL(audioBlob);
+          audioChunks = [];
+        };
+        //self.globalMediaRecorder.start();
       })
       .catch((error) => {
         console.error('Error al obtener acceso al micrófono:', error);
@@ -172,13 +240,13 @@ export class OamAssistantChatComponent implements OnInit {
   }
 
   onStopMediaRecording() {
+    this.globalMediaRecorder?.stop();
     const audioBlob = new Blob(this.globalAudioChunks, { type: 'audio/mp3' });
 
     const downloadLink = document.createElement('a');
     downloadLink.href = URL.createObjectURL(audioBlob);
     downloadLink.download = 'grabacion.mp3';
     downloadLink.click();
-    this.globalMediaRecorder?.stop();
   }
 
   getCurrentDate() {
